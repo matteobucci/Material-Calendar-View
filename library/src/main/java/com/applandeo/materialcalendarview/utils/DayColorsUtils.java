eb95537c56644c8cacf1ac099d952950bc42ecad
matteobucci <matteo.bucci.1995@gmail.com>
2019-01-03 15:30:17 +0100

    Better UI and support for circle images

--- library/src/main/java/com/applandeo/materialcalendarview/utils/DayColorsUtils.java
+++ library/src/main/java/com/applandeo/materialcalendarview/utils/DayColorsUtils.java
@@ -29,7 +29,7 @@ public class DayColorsUtils {
             return;
         }
 
-        textView.setTypeface(null, typeface);
+        textView.setTypeface(textView.getTypeface(), typeface);
         textView.setTextColor(textColor);
         textView.setBackgroundResource(background);
     }
